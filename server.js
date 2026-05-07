a0cf0a0ad66b7ca3699b569cdfa82b8770ac224c
mootez-brayek <mootez.brayek@esprit.tn>
2026-05-07 03:06:13 +0100

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const entry = path.join(__dirname, 'dist', 'server.js');
 try {
   require(entry);
 } catch (error) {
-  console.error(Failed to start Node backend from ${entry});
+  console.error(`Failed to start Node backend from ${entry}`);
   console.error(error);
   process.exit(1);
 }
